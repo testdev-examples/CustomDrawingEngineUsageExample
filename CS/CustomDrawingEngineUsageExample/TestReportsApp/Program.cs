c0f73cb0da96199a1ddef629f563da930a50e549
Andrey Lepikhov (auto) <andrey.lepikhov@devexpress.com>
2020-08-23 21:23:59 +0300

    auto sync for version 2020.2

--- CS/CustomDrawingEngineUsageExample/TestReportsApp/Program.cs
+++ CS/CustomDrawingEngineUsageExample/TestReportsApp/Program.cs
@@ -8,13 +8,9 @@ namespace TestReportsApp {
 
             var report = new Reports.HiddenColumnsReport();
 
-            // https://docs.devexpress.com/XtraReports/401730/create-end-user-reporting-applications/web-reporting/asp-net-core-reporting/use-the-devexpress-cross-platform-drawing-engine
-            if(!RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
-                DevExpress.Printing.CrossPlatform.CustomEngineHelper.RegisterCustomDrawingEngine(
-                    typeof(
-                        DevExpress.CrossPlatform.Printing.DrawingEngine.PangoCrossPlatformEngine
-                ));
-            }
+            object engine = new DevExpress.CrossPlatform.Printing.DrawingEngine.PangoCrossPlatformEngine();
+            DevExpress.XtraReports.Native.CrossPlatform
+               .CrossPlatformCustomEngineHelper.RegisterCustomEngine(report, engine);
 
             report.CreateDocument();
             report.ExportToPdf("HiddenColumnsReport.pdf");
